c106856ef4a89fcb84d372ced5c23d823338af1f
Juanjo Jiménez <juanji0503@gmail.com>
2017-08-23 02:31:50 +0200

    SmartU mockup project uploaded
    This mockups contains the first pages of the website. More work is needed.

---
+++ smartu_materialize_mockup/js/scripts.js
@@ -0,0 +1,6 @@
+$(document).ready(function(){
+    // Initialize collapse button
+    $(".button-collapse").sideNav();
+    // Initialize hover effect on dropdowns
+    $(".dropdown-button").dropdown({hover: true});
+});
